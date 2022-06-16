b4e3614eb25decdf67e98bc30ecf23796052d337
vindah <patwikeyo2@gmail.com>
2022-06-16 03:18:01 +0100

    Updated testRunner class to run test with cucumber tags.

--- src/test/java/runner/testRunner.java
+++ src/test/java/runner/testRunner.java
@@ -1,4 +1,21 @@
 package runner;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"src/test/resources/features"},
+        tags= "@test",
+        glue = {"stepDefinition"},
+        plugin = {
+                //"com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html",
+                "pretty","html:target/cucumber", "json:target/cucumber.json",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+        }
+)
+
 public class testRunner {
+
 }
