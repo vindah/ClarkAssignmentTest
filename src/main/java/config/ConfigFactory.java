443cf52108e448b8ea46910cd8bab17a63ed71b3
vindah <patwikeyo2@gmail.com>
2022-06-16 02:34:34 +0100

    1. Updated config factory file with property path. 2. Updated properties file with url and other data needed for test.

--- src/main/java/config/ConfigFactory.java
+++ src/main/java/config/ConfigFactory.java
@@ -1,5 +1,37 @@
 package config;
 
-public class ConfigFactory {
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 
+public class ConfigFactory {
+    private Properties properties;
+    //private final String propertyFilePath= "src//test//resources//Configuration.properties";
+    private final String propertyFilePath = getPropertyFilePath();
+
+
+    private String getPropertyFilePath() {
+        String profileId = System.getProperty("profileId");
+        if (profileId == null) profileId = "staging";
+        return "src//test//resources//profiles//" + profileId + "//config.properties";
+    }
+
+    public void configFileReader() {
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader(propertyFilePath));
+            properties = new Properties();
+            try {
+                properties.load(reader);
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Configuration.properties not found at " + propertyFilePath);
+        }
+    }
 }
