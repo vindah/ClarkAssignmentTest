743fee736d3834f112e0278dc705124f39da317a
vindah <patwikeyo2@gmail.com>
2022-06-16 03:01:25 +0100

    Updated driver factory and home page test with before method.

--- src/main/java/driverUtil/driverFactory.java
+++ src/main/java/driverUtil/driverFactory.java
@@ -1,4 +1,4 @@
-package driver;
+package driverUtil;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
--- src/test/java/stepDefinition/HomePageTest.java
+++ src/test/java/stepDefinition/HomePageTest.java
@@ -1,4 +1,23 @@
 package stepDefinition;
 
+import config.ConfigFactory;
+import io.cucumber.java.Before;
+import org.openqa.selenium.WebDriver;
+
+import java.util.concurrent.TimeUnit;
+
 public class HomePageTest {
+
+    WebDriver driver;
+    ConfigFactory ConfigFileReader;
+
+
+
+    // This runs before each test
+    @Before()
+    public void setup() {
+        driver = driverUtil.driverFactory.open("chrome");
+        driver.manage().timeouts().implicitlyWait(ConfigFileReader.getImplicitlyWait(), TimeUnit.SECONDS) ;
+        ConfigFileReader = new ConfigFactory();
+    }
 }
